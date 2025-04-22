851993900cf6c2c91a151d133ab24a7a4217637a
Snoofz <ryanantongiovanni0@gmail.com>
2025-04-22 16:30:24 -0700

    Delete bot directory

--- bot/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    ConnectionCredentials: require('./connection_details').ConnectionCredentials,
-    GameplayBot: require('./gameplay_bot').GameplayBot,
-    LobbyBot: require('./lobby_bot').LobbyBot,
-};
